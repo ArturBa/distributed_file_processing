21d62bd1cbc2c989cd441a6319d88b0f0c7b1d8a
jagajda <jagajda@student.agh.edu.pl>
2020-05-28 23:26:16 +0200

    Add server communication

--- src/Server.py
+++ src/Server.py
@@ -1,31 +1,73 @@
 import subprocess
 import sys
-from converter import Converter
+#from converter import Converter
 from UserCLI import *
 from Worker import *
 import time
-from multiprocessing.connection import Listener, Client
-address = (localhost,8080)
+import socket
+import threading
 
+host = '127.0.0.1'
+port = 8080
+client = socket.socket()
+
+try:
+    client.bind((host, port))
+except socket.error as e:
+    print(str(e))
+    
+client.listen(5)
+
+def parse_raw_input(msg):
+	'''De-serializing raw socket message to python object format'''
+	return pickle.loads(msg)
+    
+def parse_raw_output(msg):
+	'''Serializing python object to raw socket message'''
+	return pickle.dumps(msg)
+	
 class Server:
     def __init__ (self, mainFile, toConvertFiles = None, workerList = None, fileData = None):
         self.mainFile = mainFile
         self.toConvertFiles = toConvertFiles
         self.workerList = workerList
         self.fileData = fileData
-        self._listener = Listener(address)
-        self._client = Client(address)
 
+    def connect(self):
+        try:
+            client.accept()
+        except socket.error as e:
+            print(str(e))
+            
     def addNewWorker(self, worker = None):
         if worker is not None:
             if not self.checkIfWorkerAlreadyAdded(worker):
                 self.workerList.append(worker)
                 
+    def new_worker_connection(self, connection):
+        while True:
+            msg = client.recv()
+            msg = parse_raw_input(msg)
+            if self.parseJoinMessage(msg):
+                worker = Worker(pid = msg['pid'], qsize = msg['qsize'])
+                if not self.checkIfWorkerAlreadyAdded():
+                    self.addNewWorker(worker)
+                    client.send(self.getJoinAcceptMsg(msg))
+            elif self.parseFreeQSizeRequest(msg):
+                pass
+         
     def checkIfWorkerAlreadyAdded(self, worker):
         for i in self.workerList:
             if i == worker:
                 return True
         return False
+        
+    def checkWorkers(self):
+        while True:
+            print("Checking workers")
+            _client, address = client.accept()
+            print("Connected to {client} on address {address}")
+            new_thread = threading.Thread(target = self.new_worker_connection, args = (_client))
 
     def parseJoinMessage(self,msg):
         return msg['type'] == 'join'
@@ -34,18 +76,6 @@ class Server:
         return {'type':'join',
                 'pid': worker_request['pid'],
                 'result':'accepted'}
-
-    def checkWorkers(self, timeout=0):
-        start_time = time.time()
-        self._listener.accept()
-        while time.time() < start_time + timeout:
-            msg = self._listener.recv()
-            if self.parseJoinMessage(msg):
-                worker = Worker(pid = msg['pid'], qsize = msg['qsize'])
-                if not self.checkIfWorkerAlreadyAdded():
-                    self.addNewWorker(worker)
-                    self._client.send(self.getJoinAcceptMsg(msg))
-        return len(self.workerList)
     
     def getWorkerQSizeRequest(self, worker):
         return {'type': 'free_space_reques',
@@ -68,10 +98,11 @@ class Server:
         start_time = time.time()
         workers_served = 0
         while time.time() < start_time + timeout or workers_served > len(self.workerList):
-            msg = self._listener.recv()
+            msg = client.recv(1024)
             if self.parseFreeQsizeRequest:
                 workers_served += 1
-        return len(any[worker._free_qsize > 0 for worker in self.workerList])
+        #return len(any[(worker._free_qsize > 0) for worker in self.workerList])
+        return 1
 
     def splitFile (self):
         cmd = ['python', 'videoSplit.py', '-f', self.mainFile.location, '-s', '10', '-v', 'h264']
@@ -107,9 +138,11 @@ class Server:
 user = UserCLI()
 user.setFileData()
 server = Server(user.fileData)
-if server.splitFile():
-    print("File successfully splitted")
-else:
-    print("Error while splitting file")
-    sys.exit(-1)
-server.convertFile()
+listener_thread = threading.Thread(server.checkWorkers())
+listener_thread.start()
+#if server.splitFile():
+    #print("File successfully splitted")
+#else:
+    #print("Error while splitting file")
+    #sys.exit(-1)
+#server.convertFile()
